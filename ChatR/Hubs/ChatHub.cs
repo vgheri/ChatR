8d49033c2bb90b427a1a7bfc3b9be62f1319ceb3
Valerio Gheri <valerio.gheri@gmail.com>
2012-07-13 17:26:57 +0200

    Added the hub and missing dlls

---
+++ ChatR/Hubs/ChatHub.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SignalR;
+using SignalR.Hubs;
+
+namespace ChatR.Hubs
+{
+    public class ChatHub : Hub
+    {
+
+    }
+}
