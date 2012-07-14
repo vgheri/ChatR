923d88b582bfe9639512bda351cd7f0a0472b483
Valerio.Gheri <valerio.gheri@gmail.com>
2012-07-14 13:11:51 +0200

    Further enhancements to project structure and Hub
    Added missing files from the whole project structure.
    Added Chat event handlers to the Hub object.
    Renamed model because of naming conflict with SignalR

--- ChatR/Hubs/ChatHub.cs
+++ ChatR/Hubs/ChatHub.cs
@@ -4,11 +4,39 @@ using System.Linq;
 using System.Web;
 using SignalR;
 using SignalR.Hubs;
+using ChatR.Models;
 
 namespace ChatR.Hubs
 {
-    public class ChatHub : Hub
+    public class ChatHub : Hub, IDisconnect, IConnected
     {
+        #region IDisconnect and IConnected event handlers implementation
 
+        public System.Threading.Tasks.Task Disconnect()
+        {
+            return Clients.leaves(Context.ConnectionId, Caller.userName, DateTime.Now);
+        }
+
+        public System.Threading.Tasks.Task Connect()
+        {
+            return Clients.joined(Context.ConnectionId, Caller.userName, DateTime.Now);
+        }
+
+        public System.Threading.Tasks.Task Reconnect(IEnumerable<string> groups)
+        {
+            return Clients.rejoined(Context.ConnectionId, Caller.userName, DateTime.Now);
+        }
+
+        #endregion
+
+        #region Chat event handlers
+
+        public void Send(ChatMessage message)
+        {
+            message.Timestamp = DateTime.Now;
+            Clients.onMessageReceived(message);
+        }
+
+        #endregion
     }
 }
---
+++ ChatR/Models/ChatMessage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChatR.Models
+{
+    public class ChatMessage
+    {
+        public string Username { get; set; }
+        public string Message { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
