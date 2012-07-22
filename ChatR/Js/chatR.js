4613acb52a0dc942787cf8df6fe64b3f2ac640cf
Valerio.Gheri <valerio.gheri@gmail.com>
2012-07-22 18:36:39 +0200

    Added js namespace
    Added a new js file (and folder) to introduce a js namespace, chatR.
    I kept the ready() handler in the chat.cshtml file as I need data from
    the ViewBag

---
+++ ChatR/Js/chatR.js
@@ -0,0 +1,35 @@
+﻿var chatR = {};
+
+// Models
+chatR.ChatMessage = function (sender, content, dateSent) {
+    var self = this;
+    self.Username = sender;
+    self.Message = content;
+    if (dateSent != null) {
+        self.Timestamp = dateSent;
+    }
+}
+
+chatR.User = function (username) {
+    var self = this;
+    self.username = username;
+}
+
+// ViewModels
+
+chatR.ChatViewModel = function () {
+    var self = this;
+    self.messages = ko.observableArray();
+}
+
+chatR.ConnectedUsersViewModel = function () {
+    var self = this;
+    self.contacts = ko.observableArray();
+    self.CustomRemove = function (userToRemove) {
+        var usernameToRemove = userToRemove.username;
+        self.contacts.remove(function (item) {
+            return item.username === usernameToRemove;
+        });
+    }
+}
+
